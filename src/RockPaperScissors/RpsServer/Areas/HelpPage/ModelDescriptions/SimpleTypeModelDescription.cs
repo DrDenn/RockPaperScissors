37e8f7faa0d10865802ed7aefd8c4b23b4838363
Brandon <shadowdenn@gmail.com>
2017-05-21 21:50:01 -0700

    Updated to Server to Empty project (#1)
    Squash and Merge

--- src/RockPaperScissors/RpsServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RpsServer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- src/RockPaperScissors/RpsServer/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RpsServer.WebApiApplication" Language="C#" %>
