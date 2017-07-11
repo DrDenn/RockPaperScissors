86a5c0d23105e2b2219c8be440965edc45783bca
Skyler <goodsky6@gmail.com>
2017-07-10 21:24:12 -0700

    Gulp runner for javascript.

---
+++ src/RockPaperScissors/RpsWebsite/gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding BeforeBuild='default' />
+
+var gulp = require('gulp');
+
+var project = {
+    webroot: "wwwroot"
+};
+
+var paths = {
+    webroot: "./" + project.webroot + "/",
+    lib: "./" + project.webroot + "/lib/",
+    scripts: "./" + project.webroot + "/lib/scripts/"
+};
+
+gulp.task('default', function () {
+    gulp.src("./Scripts/*.js")
+        .pipe(gulp.dest(paths.scripts));
+});
