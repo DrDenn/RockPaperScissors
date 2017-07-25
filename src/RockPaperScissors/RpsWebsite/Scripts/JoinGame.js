13dd2fd5b2480a5fa77ad5ccf0308d43bc297578
Skyler <goodsky6@gmail.com>
2017-07-24 22:32:02 -0700

    Use JoinGame.js from Play Action index.html

--- src/RockPaperScissors/RpsWebsite/Scripts/JoinGame.js
+++ src/RockPaperScissors/RpsWebsite/Scripts/JoinGame.js
@@ -1,1 +1,7 @@
-﻿$document()
+﻿function joinGame(endpoint) {
+    var joinGameUrl = endpoint + "/api/rps";
+    $.get(joinGameUrl, function (data, status) {
+        document.getElementById("response").innerHTML = "<p>" + data + "</p>";
+        window.location.href = "/Play/InGame?gameId=" + data;
+    });
+}
